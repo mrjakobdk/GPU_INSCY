f8d6b0f1824f57ecbbc461fbf69fef27a3b5cc03
mrjakobdk <mrjakobdk@gmail.com>
2020-07-23 15:13:29 +0200

    made redundancy and join parallel  - got at small speedup by removing syncs

--- experiments/test.py
+++ experiments/test.py
@@ -23,7 +23,6 @@ print("Finished loading dataset, took: %.4fs" % (time.time() - t0))
 print()
 X_ = X[:params["n"], :params["subspace_size"]].clone()
 
-
 # t0 = time.time()
 # subspaces, clusterings = INSCY.CPU(X_, params["neighborhood_size"], params["F"],
 #                                    params["num_obj"], params["min_size"], r=params["r"],
@@ -33,15 +32,14 @@ X_ = X[:params["n"], :params["subspace_size"]].clone()
 # print("number of clusters", INSCY.count_number_of_clusters(subspaces, clusterings))
 # print("clustered points", np.count_nonzero(np.array(clusterings)>=0))
 
-
-# t0 = time.time()
-# subspaces, clusterings = INSCY.GPU3(X_, params["neighborhood_size"], params["F"],
-#                                     params["num_obj"], params["min_size"], r=params["r"],
-#                                     number_of_cells=params["number_of_cells"], rectangular=True,
-#                                     entropy_order=0)
-# print("Finished GPU3-INSCY random, took: %.4fs" % (time.time() - t0))
-# print("number of clusters", INSCY.count_number_of_clusters(subspaces, clusterings))
-# print("clustered points", np.count_nonzero(np.array(clusterings)>=0))
+t0 = time.time()
+subspaces, clusterings = INSCY.GPU3(X_, params["neighborhood_size"], params["F"],
+                                    params["num_obj"], params["min_size"], r=params["r"],
+                                    number_of_cells=params["number_of_cells"], rectangular=True,
+                                    entropy_order=0)
+print("Finished GPU3-INSCY random, took: %.4fs" % (time.time() - t0))
+print("number of clusters", INSCY.count_number_of_clusters(subspaces, clusterings))
+print("clustered points", np.count_nonzero(np.array(clusterings)>=0))
 
 
 t0 = time.time()
